3c69c2d65e334daaf7f69472192dc6e0d207b398
Andres-ibanez <cristian.ibanez@aluno.unila.edu.br>
2020-11-01 04:42:51 -0300

    Create AppRouter

---
+++ rickandmorty/src/routers/AppRouter.js
@@ -0,0 +1,30 @@
+
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+  
+  import { MainScreen } from '../components/MainScreen';
+  import { CharacterScreen } from '../components/CharacterScreen';
+
+  import GlobalStyles from '../styles/global';
+
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Router>
+                <div>
+
+                    <Switch>
+                        <Route exact path="/" component={ MainScreen }/>
+                        <Route exact path="/:characterId" component={ CharacterScreen } />
+                    </Switch>
+                </div>
+            </Router>
+            <GlobalStyles />
+        </>
+    )
+}
