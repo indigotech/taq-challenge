b458f1edb185a7d6f59e3845bf184fb29423668f
Andres-ibanez <cristian.ibanez@aluno.unila.edu.br>
2020-11-01 02:02:10 -0300

    Add global Style

---
+++ rickandmorty/src/styles/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  html, body {
+    height: 100%;
+    background-color: #282a36;
+    color: #f8f8f2
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+`
+
+export default GlobalStyles
