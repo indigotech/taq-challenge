c4176d425bcaba145160b32032d63106bf8d6e25
iuri-unb <160152488@aluno.unb.br>
2021-09-05 09:26:43 -0300

    first commit

---
+++ taq-challenge/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Ricky and Morty</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
