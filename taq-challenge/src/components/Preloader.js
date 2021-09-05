cffabe31109ed1875039a8994db86b44d67b972c
iuri-unb <160152488@aluno.unb.br>
2021-09-05 15:32:49 -0300

    preloader component

---
+++ taq-challenge/src/components/Preloader.js
@@ -0,0 +1,11 @@
+import { LoaderContainer } from "../styles/Loader-style"
+import { Ring } from 'react-awesome-spinners'
+
+function Preloader() {
+  return(
+    <LoaderContainer>
+      <Ring />
+    </LoaderContainer>
+  )}
+
+export default Preloader
