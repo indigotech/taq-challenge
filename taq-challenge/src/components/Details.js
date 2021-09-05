36ef2aedb77e4f3401cd919b0c9af4e83899fae0
iuri-unb <160152488@aluno.unb.br>
2021-09-05 15:32:36 -0300

    character details template

--- taq-challenge/src/components/Details.js
+++ taq-challenge/src/components/Details.js
@@ -6,16 +6,15 @@ import {
 } from "@apollo/client";
 import toast from "react-hot-toast";
 
-import styled from 'styled-components';
+import { FiChevronLeft } from 'react-icons/fi';
+import { DetailsContainer, HeadContainer, HeadContent, Status, EpContainer } from "../styles/Details-style";
+import { BackContainer } from "../styles/BackButton-style";
+import Preloader from "../components/Preloader";
 
-const DetailsContainer = styled.div`
-  padding: 2em;
-  flex-flow: wrap;
-  padding: 2em;
-`;
 
 function Details(props){
   const [details, setDetails] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   const client = new ApolloClient({
     uri: 'https://rickandmortyapi.com/graphql',
@@ -42,8 +41,10 @@ function Details(props){
       `
     }).then(response => {
       setDetails(response.data.character);
+      setIsLoading(false)
     }).catch(error => {
       console.error(error)
+      setIsLoading(false)
       toast.error('Cannot get character details, try again later!')
     });
   }
@@ -53,21 +54,34 @@ function Details(props){
   };
 
   return(
-    <DetailsContainer>
-      <button onClick={handleClick}>Back</button>
-      {details &&
-        <span>
-          <img alt={details.name} src={details.image} />
-          <h1>{details.name}</h1>
-          <p>{details.status}</p>
-          <h2>Episodes</h2>
-          {details.episode.map((ep, index) => (
-            <p key={index}>{ep.name}</p>
-          ))}
-        </span>
+    isLoading ? <Preloader /> : (
+      <DetailsContainer>
+        <BackContainer onClick={handleClick}>
+          <FiChevronLeft size={20}/>Back
+        </BackContainer>
+        {details &&
+          <span>
+            <HeadContainer>
+              <img alt={details.name} src={details.image} />
+              <HeadContent>
+                <h1>{details.name}</h1>
+                {details.status === 'Alive' ?
+                  <Status primary>{details.status}</Status>
+                  : (details.status === 'Dead' ? <Status secondary>{details.status}</Status> : <Status>{details.status}</Status>)
+                }
+              </HeadContent>
+            </HeadContainer>
+            <h2>Episodes</h2>
+            {details.episode.map((ep, index) => (
+              <EpContainer>
+                <p key={index}>{ep.name}</p>
+              </EpContainer>
+            ))}
+          </span>
 
-      }
-    </DetailsContainer>
+        }
+      </DetailsContainer>
+    )
   );
 }
 
